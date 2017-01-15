b7b981ac7bd9009e672c72ca86f1a59264d6c30f
spidercrew <baimek88@gmail.com>
2017-01-15 15:07:34 +0700

    Create popularpost.js

---
+++ popularpost.js
@@ -0,0 +1,3 @@
+//<![CDATA[
+function resizeThumbarlina(e,t){for(var s=document.getElementById(e),r=s.getElementsByTagName("img"),c=0;c<r.length;c++)r[c].src=r[c].src.replace(/\/s72\-c/,"/s"+t),r[c].width=t,r[c].height=t}resizeThumbarlina("PopularPosts1",400);
+//]]>
