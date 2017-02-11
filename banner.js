4753d8628afe80c3a97769bfa04a4af972a6d9bc
spidercrew <baimek88@gmail.com>
2017-02-11 23:19:45 +0700

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,35 @@
+<script type="text/javascript">
+var arldgzn_id = 3;
+var arldgzn_now = new Date()
+var detik = arldgzn_now.getSeconds()
+var arldgzn_place = detik % arldgzn_id;
+arldgzn_place += 1;
+if (arldgzn_place == 1) {
+  url = "/2016/12/tournament-rake-race-poker-online-idnplay-hot-event-desember.html";
+  alt = "Tornament Rake Race Poker Online IDNPlay ( Hot Event !! January )";
+  title = "Tornament Rake Race Poker Online IDNPlay ( Hot Event !! January )";
+  banner = "https://2.bp.blogspot.com/-tzCekg7JinY/WJNdKhjzsjI/AAAAAAAADQI/JlY4-TlV7_YkUyagI1ZhRN169_yzhxhKACLcB/s1600/rakerace-id.png";
+  width = "640";
+  height = "125";
+}
+if (arldgzn_place == 2) {
+  url = "/2016/12/renpokercom-poker-online-terpercaya-dan-terbesar-di-asia.html";
+  alt = "Free Chip Gratis Setiap Harinya";
+  title = "Free Chip Gratis Setiap Harinya";
+  banner = "https://1.bp.blogspot.com/-XkcesgPAtkU/WJxQ_NO5SlI/AAAAAAAADUs/nm3erDHi8HkXTacxm2_C8EzmhMPvAaVgACLcB/s320/Situs%2BPoker%2BOnline%2BIndonesia%2BUang%2BAsli.jpeg";
+  width = "640";
+  height = "125";
+}
+if (arldgzn_place == 3) {
+  url = "https://infopreman.blogspot.co.id/2017/01/isopoker-situs-permainan-poker-dan-domino-online-dengan-pelayanan-terbaik.html";
+  alt = "Infopokerku | Poker Sakti";
+  title = "Infopokerku | Poker Sakti";
+  banner = "https://1.bp.blogspot.com/-2QljqhsT-l4/WJy4bl2YPiI/AAAAAAAADVs/plKocFPwvAM0Q4nFNcdmCVAAfx2PrShIQCLcB/s320/U9YDvhx.jpg";
+  width = "640";
+  height = "125";
+}
+document.write('<a href=\"' + url + '\" target=\"_blank\" rel=\"nofollow\">');
+document.write('<img src=\"' + banner + '\" width=')
+document.write(width + ' height=' + height + ' ');
+document.write('title=\"' + title + '\" alt=\"' + alt + '\"></a>');
+</script>
